4b3c6ba3b820d5991f5aa8c4334b33a08410bfc2
Vladimir <balaian.vladimir@yandex.ru>
2023-01-22 23:25:26 +0300

    тест хаски_2

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import Sinon from 'sinon';
+import Sinon from "sinon";
 
-export const getContentFake = Sinon.fake.returns(document.createElement('div'));
+export const getContentFakeR = Sinon.fake.returns(
+  document.createElement("div")
+);
 
 export const BlockMock = class {
   getContent = getContentFake;
